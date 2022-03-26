eb250dcdcfc66b3a805768d17d1ceffce3e95587
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-03-27 00:32:13 +0300

    Initializing configes tailwind, react, styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'gilroy': ['Another Danger Slanted', 'system-ui'],
+    },
+    colors: {
+      purple: "#7D1ABA",
+      blue: "#3F65EC",
+      semiblue:"#8952FF",
+      white:'#FFFFFF',
+    },
   },
   plugins: [],
 }
