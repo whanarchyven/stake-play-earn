1b3f94d964f4c94f78cf7b4952714abf254ca01a
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-03-27 15:29:31 +0300

    1st block done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NeonButton from "../components/NeonButton";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,64 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={'text-purple font-gilroy font-extrabold text-9xl'}> Hello, World!</h1>
+          <div className={'w-[100vw] relative block h-[61vw] bg-cover bg-[url("../public/images/main.png")]'}>
+
+              <div className={'z-50 justify-between justify-items-center items-center w-[38.4vw] h-[2vw] absolute inline-flex left-[29.7vw] top-[11.1vw]'}>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>nft marketplace</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>metaverse</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>swap</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>TEASER</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>DAO</a>
+              </div>
+              <div className={'z-50 justify-between justify-items-center items-center w-[28vw] h-[2vw] absolute inline-flex left-[33.6vw] top-[13.1vw]'}>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>Stake</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>TEAM</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>ROADMAP</a>
+                  <a className={'cursor-pointer text-white text-center uppercase text-[1.2vw] font-gilroy font-regular'}>PARTNERS</a>
+              </div>
+              <div className={'justify-items-center justify-center items-center absolute inline-flex left-[71.2vw] top-[11.2vw] w-[12.5vw] h-[3.4vw] rounded-full bg-purple'}>
+                  <h2 className={'text-white text-center uppercase text-[1.1vw] font-gilroy font-bold'}>Connect wallet</h2>
+              </div>
+
+
+              <div className={'justify-items-center items-center justify-align-center w-[22vw] h-[5.5vw] left-[28.3vw] top-[21.3vw] rounded-full absolute inline-flex offer-gradient'}>
+                  <h2 className={'w-full text-center text-white font-gilroy font-extrabold text-[2.2vw]'}>122 450% APY</h2>
+              </div>
+              <div className={'w-[57vw] h-[53vw] absolute inline-block bottom-0 left-0'}>
+                  <Image src={'/images/phone.png'} layout={'fill'}></Image>
+              </div>
+              <h1 className={'tracking-[0.015em] absolute right-[11.5vw] text-[4.72vw] top-[29.8vw] text-right text-white font-gilroy font-black'}>STAKE PLAY EARN</h1>
+              <h2 className={'absolute right-[11.5vw] text-[2.2vw] top-[27.2vw] text-right text-white font-gilroy font-regular'}>First DeFi & Metaverse platform</h2>
+              <div className={'cursor-pointer absolute inline-block left-[48vw] top-[38.4vw] w-[14.5vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'Stake $Token'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer absolute inline-block left-[64vw] top-[38.4vw] w-[12.4vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'Metaverse'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer absolute inline-block left-[78.2vw] top-[38.4vw] w-[8.2vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'NFT'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer absolute inline-block left-[50.7vw] top-[43.6vw] w-[8.2vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'DEX'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer absolute inline-block left-[61.1vw] top-[43.6vw] w-[8vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'DAO'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer absolute inline-block left-[71.5vw] top-[43.6vw] w-[13.5vw] h-[3.6vw] text-[1.7vw] rounded-[20px]'}>
+                  <NeonButton title={'Play-2-Earn'}></NeonButton>
+              </div>
+              <div className={'cursor-pointer w-[2.3vw] h-[2.3vw] bottom-[10.4vw] left-[14.45vw] absolute inline-block'}>
+                  <Image src={'/images/twitter.svg'} layout={'fill'}></Image>
+              </div>
+              <div className={'cursor-pointer w-[2.3vw] h-[2.5vw] bottom-[10.4vw] left-[17.45vw] absolute inline-block'}>
+                  <Image src={'/images/discord.svg'} layout={'fill'}></Image>
+              </div>
+              <div className={'cursor-pointer w-[2.3vw] h-[2.3vw] bottom-[10.4vw] left-[20.5vw] absolute inline-block'}>
+                  <Image src={'/images/telegram.svg'} layout={'fill'}></Image>
+              </div>
+          </div>
+
+
       </main>
 
       <footer className={styles.footer}>
