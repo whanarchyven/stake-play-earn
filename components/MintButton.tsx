4f7f80eec1c88c0426a847f7bb8578b848eb089d
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-05-17 09:49:08 +0300

    Switch on mint buttons

--- components/MintButton.tsx
+++ components/MintButton.tsx
@@ -18,18 +18,18 @@ export const MintButton = ({category}:{category:string}) => {
     }
 
 
-    // <button
-    //     onClick={onClick}
-    //     className={'cursor-pointer w-[75%] mx-[12.5%] mt-[4vw] h-[3.5vw] text-[1.8vw] bg-[#C4C4C4] text-white rounded-full font-gilroy font-bold'}>
-    //
-    //     {mintState==="none"?`MINT`:""}
-    //     {mintState==="loading"?"Minting...":""}
-    //     {mintState==="success"?"MINTED!":""}
-    //     {mintState==="error"?"error":""}
-    // </button>
-
     return <button
-        className={'cursor-pointer w-[100%] mt-[1vh] sm:w-[75%] text-[2vh] sm:mx-[12.5%] h-[2.5vh] sm:mt-[4vw] sm:h-[3.5vw] sm:text-[1.8vw] bg-[#C4C4C4] text-white rounded-full font-gilroy font-bold'}>
-        SOON
+        onClick={onClick}
+        className={'cursor-pointer w-[75%] mx-[12.5%] mt-[4vw] h-[3.5vw] text-[1.8vw] bg-[#C4C4C4] text-white rounded-full font-gilroy font-bold'}>
+
+        {mintState==="none"?`MINT`:""}
+        {mintState==="loading"?"Minting...":""}
+        {mintState==="success"?"MINTED!":""}
+        {mintState==="error"?"error":""}
     </button>
+
+    // return <button
+    //     className={'cursor-pointer w-[100%] mt-[1vh] sm:w-[75%] text-[2vh] sm:mx-[12.5%] h-[2.5vh] sm:mt-[4vw] sm:h-[3.5vw] sm:text-[1.8vw] bg-[#C4C4C4] text-white rounded-full font-gilroy font-bold'}>
+    //     SOON
+    // </button>
 }
